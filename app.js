929fb01b974db5bbea6323c3a226d29abc14f525
KITAHARA toshihiko <snow227@albirex2006.com>
2017-10-18 05:28:26 +0900

    コミット

--- app.js
+++ app.js
@@ -34,10 +34,10 @@ rl.on('close', () => {
         value.change = value.popu15 / value.popu10;
     }
     const ranklingArray = Array.from(map).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+        return -(pair2[1].change - pair1[1].change);
     });
-    const rankingString = ranklingArray.map((pair) => {
-        return pair[0] + ':' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
+    const rankingString = ranklingArray.map((pair, rank) => {
+        return rank+1 + ':' + pair[0] + ':' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
     });
     console.log(rankingString);
 });
